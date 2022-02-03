8e4e09d5c35cf1a5eea026fd69a5f023076515cf
Brian Neeland <brian.neeland@protonmail.com>
2022-02-02 18:19:06 -0700

    Passing basic test

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,12 +5,12 @@ import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
   address payable[] public players;
-  uint256 public usdEntryFree;
+  uint256 public usdEntryFee;
   AggregatorV3Interface internal ethUsdPriceFeed;
 
 
   constructor(address _priceFeedAddress) public {
-    usdEntryFree = 50 * 10 ** 18;
+    usdEntryFee = 50 * 10**18;
     ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
   }
 
@@ -20,13 +20,13 @@ contract Lottery {
   }
 
   function getEntranceFee() public view returns (uint256) {
-    (, int price, ,,,) = ethUsdPriceFeed.latestRoundData;
-    uint256 adjustedPrice = uint256(price) * 10 ** 10; // 18 decimals
-    uint256 costToEnter = (usdEntryFee * 10 ** 18) / price;
+    (, int price, , , ) = ethUsdPriceFeed.latestRoundData();
+    uint256 adjustedPrice = uint256(price) * 10**10; // 18 decimals
+    uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
     return costToEnter;
   }
 
   function startLottery() public {}
-  
+
   function endLottery() public {}
 }
---
+++ tests/test_lottery.py
@@ -0,0 +1,11 @@
+from brownie import Lottery, accounts, config, network
+from web3 import Web3
+
+def test_get_entrance_fee():
+    account = accounts[0]
+    lottery = Lottery.deploy(
+        config["networks"][network.show_active()]["eth_usd_price_feed"],
+        {"from": account},
+    )
+    assert lottery.getEntranceFee() > Web3.toWei(0.017, "ether") # 50 USD / 2721.41 ETH = 0.0184
+    assert lottery.getEntranceFee() < Web3.toWei(0.021, "ether") # 50 USD / 2721.41 ETH = 0.0184
