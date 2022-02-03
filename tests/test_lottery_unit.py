8311f0f6addda4893a677fd0a01e0d535c839daa
Brian Neeland <brian.neeland@protonmail.com>
2022-02-03 13:37:19 -0700

    Added skip for entrance fee test if not in local network

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -1,8 +1,12 @@
 from brownie import Lottery, accounts, config, network
 from scripts.deploy_lottery import deploy_lottery
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from web3 import Web3
+import pytest
 
 def test_get_entrance_fee():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
     # Arrange
     lottery = deploy_lottery()
     # Act
